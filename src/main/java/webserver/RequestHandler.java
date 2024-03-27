d048cde03b1882fa1f8132ad2e802086727517b1
정소민 <yjk70702002@gmail.com>
2024-03-27 17:46:44 +0900

    Feat: 요구사항 2.5 - 분기문 처리

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,6 +1,5 @@
 package webserver;
 
-import controller.*;
 import http.util.HttpRequest;
 import http.util.HttpResponse;
 
@@ -9,15 +8,9 @@ import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import static http.enumclass.HttpMethod.GET;
-import static http.enumclass.HttpMethod.POST;
-import static http.enumclass.HttpUrl.*;
-
 public class RequestHandler implements Runnable{
     Socket connection;
     private static final Logger log = Logger.getLogger(RequestHandler.class.getName());
-    Controller controller = new ForwardController();
-
     public RequestHandler(Socket connection) {
         this.connection = connection;
     }
@@ -32,36 +25,8 @@ public class RequestHandler implements Runnable{
             HttpRequest httpRequest = HttpRequest.from(br);
             HttpResponse httpResponse = new HttpResponse(dos);
 
-            // 요구사항 1.1 - index.html 반환하기  // 요구사항 1.7 - CSS 출력
-            if (httpRequest.getMethod().equals(GET.getMethod()) && httpRequest.getUrl().endsWith(HTML.getUrl()) || httpRequest.getUrl().endsWith(CSS.getUrl())) {
-                controller = new ForwardController();
-            }
-
-            if (httpRequest.getMethod().equals(GET.getMethod()) && httpRequest.getUrl().equals(HOME.getUrl())) {
-                controller = new HomeController();
-            }
-
-            // 요구사항 1.2 - GET 방식으로 회원가입하기
-//            if (httpRequest.getMethod().equals(GET.getMethod()) && httpRequest.getUrl().startsWith(SIGNUP.getUrl())) {
-//                controller = new SignupController(repository);
-//            }
-
-            // 요구사항 1.3 - POST 방식으로 회원가입하기
-            if (httpRequest.getMethod().equals(POST.getMethod()) && httpRequest.getUrl().startsWith(SIGNUP.getUrl())) {
-                controller = new SignupController();
-            }
-
-            // 요구사항 1.5 - 로그인하기
-            if (httpRequest.getMethod().equals(POST.getMethod()) && httpRequest.getUrl().startsWith(LOGIN.getUrl())) {
-                controller = new LoginController();
-            }
-
-            // 요구사항 1.6 - 사용자 목록 출력
-            if (httpRequest.getMethod().equals(GET.getMethod()) && httpRequest.getUrl().startsWith(USERLIST.getUrl())) {
-                controller = new ListController();
-            }
-
-            controller.execute(httpRequest, httpResponse);
+            RequestMapper requestMapper = new RequestMapper(httpRequest, httpResponse);
+            requestMapper.proceed();
         } catch (IOException e) {
             log.log(Level.SEVERE,e.getMessage());
         }
---
+++ src/main/java/webserver/RequestMapper.java
@@ -0,0 +1,34 @@
+package webserver;
+
+import controller.*;
+import http.util.HttpRequest;
+import http.util.HttpResponse;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static http.enumclass.HttpUrl.*;
+
+public class RequestMapper {
+    private final HttpRequest httpRequest;
+    private final HttpResponse httpResponse;
+    private final Map<String, Controller> controllers = new HashMap<>();
+
+    public RequestMapper(HttpRequest httpRequest, HttpResponse httpResponse) {
+        this.httpRequest = httpRequest;
+        this.httpResponse = httpResponse;
+
+        //url, controller가 key value 형태로 저장
+        controllers.put(HOME.getUrl(), new HomeController());
+        controllers.put(SIGNUP.getUrl(), new SignupController());
+        controllers.put(LOGIN.getUrl(), new LoginController());
+        controllers.put(USERLIST.getUrl(), new ListController());
+    }
+    public void proceed() {
+        Controller controller = controllers.get(httpRequest.getUrl());
+        if (controller == null) {
+            controller = new ForwardController();
+        }
+        controller.execute(httpRequest, httpResponse);
+    }
+}
