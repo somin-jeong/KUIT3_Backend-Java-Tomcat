cdbfee59f3d824f949ee46c115de95b105526c29
정소민 <yjk70702002@gmail.com>
2024-03-30 23:50:24 +0900

    Refactor: RequestMapper 리팩토링

--- src/main/java/webserver/RequestMapper.java
+++ src/main/java/webserver/RequestMapper.java
@@ -12,23 +12,29 @@ import static http.enumclass.HttpUrl.*;
 public class RequestMapper {
     private final HttpRequest httpRequest;
     private final HttpResponse httpResponse;
-    private final Map<String, Controller> controllers = new HashMap<>();
+    private static final Map<String, Controller> controllers = new HashMap<>();
+    private final Controller controller;
 
     public RequestMapper(HttpRequest httpRequest, HttpResponse httpResponse) {
         this.httpRequest = httpRequest;
         this.httpResponse = httpResponse;
+        if (httpRequest.getUrl().endsWith(HTML.getUrl()) || httpRequest.getUrl().endsWith(CSS.getUrl())) {
+            controller = new ForwardController();
+        } else {
+            controller = controllers.get(httpRequest.getUrl());
+        }
+    }
 
+    static {
         //url, controller가 key value 형태로 저장
         controllers.put(HOME.getUrl(), new HomeController());
         controllers.put(SIGNUP.getUrl(), new SignupController());
         controllers.put(LOGIN.getUrl(), new LoginController());
         controllers.put(USERLIST.getUrl(), new ListController());
     }
+
     public void proceed() {
-        Controller controller = controllers.get(httpRequest.getUrl());
-        if (controller == null) {
-            controller = new ForwardController();
-        }
+        System.out.println("controller = " + controller);
         controller.execute(httpRequest, httpResponse);
     }
 }
